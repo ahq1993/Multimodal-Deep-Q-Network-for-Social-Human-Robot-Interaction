36272f643dcef51113ebe8e0fd75e7accb41ec6f
Ahmed Hussain Qureshi <ahqureshi1993@gmail.com>
2017-01-29 14:07:06 +0900

    Delete makefiles.lua

--- makefiles.lua
+++
@@ -1,7 +0,0 @@
-local e={}
-episode=1
-torch.save('files/reward_history.dat',e)
-torch.save('files/action_history.dat',e)
-torch.save('files/ep_rewards.dat',e)  
-torch.save('files/episode.dat',episode)
-torch.save('files/episode.txt',episode,'ascii')
